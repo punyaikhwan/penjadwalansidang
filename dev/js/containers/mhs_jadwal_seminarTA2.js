16b8af619fa7eaaa825bee5f82fea5daf2b7b342
Hasna <hasnurk@gmail.com>
2017-08-14 11:16:17 +0700

    fix some bugs

--- dev/js/containers/mhs_jadwal_seminarTA2.js
+++ dev/js/containers/mhs_jadwal_seminarTA2.js
@@ -156,9 +156,9 @@ class mhs_jadwal_seminarTA1 extends Component {
           </div>
           <hr/>
           <p className="menuTitle">Jadwal</p>
-          <MenuItem insetChildren={true} href="/mhs_jadwal_seminarKP" style={{backgroundColor:'#b0bec5'}}>Seminar KP</MenuItem>
+          <MenuItem insetChildren={true} href="/mhs_jadwal_seminarKP">Seminar KP</MenuItem>
           <MenuItem insetChildren={true} href="/mhs_jadwal_seminarTA1">Seminar TA 1</MenuItem>
-          <MenuItem insetChildren={true} href="/mhs_jadwal_seminarTA2">Seminar TA 2</MenuItem>
+          <MenuItem insetChildren={true} href="/mhs_jadwal_seminarTA2" style={{backgroundColor:'#b0bec5'}}>Seminar TA 2</MenuItem>
           <MenuItem insetChildren={true} href="/mhs_jadwal_sidangTA">Sidang Akhir</MenuItem>
           <hr/>
           <MenuItem href="/mhs_profile">Profil</MenuItem>
--- dev/js/containers/mhs_jadwal_sidangTA.js
+++ dev/js/containers/mhs_jadwal_sidangTA.js
@@ -162,10 +162,10 @@ class mhs_jadwal_seminarTA1 extends Component {
           </div>
           <hr/>
           <p className="menuTitle">Jadwal</p>
-          <MenuItem insetChildren={true} href="/mhs_jadwal_seminarKP" style={{backgroundColor:'#b0bec5'}}>Seminar KP</MenuItem>
+          <MenuItem insetChildren={true} href="/mhs_jadwal_seminarKP">Seminar KP</MenuItem>
           <MenuItem insetChildren={true} href="/mhs_jadwal_seminarTA1">Seminar TA 1</MenuItem>
           <MenuItem insetChildren={true} href="/mhs_jadwal_seminarTA2">Seminar TA 2</MenuItem>
-          <MenuItem insetChildren={true} href="/mhs_jadwal_sidangTA">Sidang Akhir</MenuItem>
+          <MenuItem insetChildren={true} href="/mhs_jadwal_sidangTA" style={{backgroundColor:'#b0bec5'}}>Sidang Akhir</MenuItem>
           <hr/>
           <MenuItem href="/mhs_profile">Profil</MenuItem>
 
