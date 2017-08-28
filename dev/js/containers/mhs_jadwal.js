d035b291ba3704133c77697e7572a373ca06fcdc
Ikhwanul Muslimin <ikhwan.m1996@gmail.com>
2017-08-28 22:45:50 +0700

    satuin seluruh jadwal mhs

---
+++ dev/js/containers/mhs_jadwal.js
@@ -0,0 +1,420 @@
+import React, {Component} from 'react';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import '../../scss/mahasiswa.scss';
+import Row from 'muicss/lib/react/row';
+import Col from 'muicss/lib/react/col';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import dateFormat from 'dateformat';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+import imgProfile from '../../scss/public/images/imgprofile.jpg';
+
+class mhs_jadwal extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      userInfo: {
+       id: "hfvcggf",
+       nama: "Ikhwanul Muslimin",
+       email: "13514020@std.stei.itb.ac.id",
+       peran: "Mahasiswa"
+      },
+      myDataSeminarKP: {
+        rincian: {
+          idKelompok: 1,
+          topik: "Implementasi X untuk Y",
+          nama: "Ikhwanul Muslimin",
+          nim: 13514020,
+          dosenPembimbing: [
+            'Rinaldi Munir',
+            'Mesayu'
+          ]
+        },
+        jadwal: {
+          dateTimeStart: "2010-06-09T15:20:00+07:00",
+          dateTimeEnd: "2010-06-09T16:20:00+07:00",
+          ruang: "7601"
+        }
+      },
+      myDataSeminarTA1: {
+        rincian: {
+          topik: "Implementasi X untuk Y",
+          nama: "Ikhwanul Muslimin",
+          nim: 13514020,
+          dosenPembimbing: [
+            'Rinaldi Munir',
+            'Mesayu'
+          ],
+          dosenPengujiTA1: [
+            'Inggriani Liem',
+            'Bayu Hendrajaya'
+          ]
+        },
+        jadwal: {
+          dateTimeStart: "2010-06-09T15:20:00+07:00",
+          dateTimeEnd: "2010-06-09T16:20:00+07:00",
+          ruang: "7602"
+        }
+      },
+      myDataSeminarTA2: {
+        rincian: {
+          topik: "Implementasi X untuk Y",
+          nama: "Ikhwanul Muslimin",
+          nim: 13514020,
+          dosenPembimbing: [
+            'Rinaldi Munir',
+            'Mesayu'
+          ],
+        },
+        jadwal: {
+          dateTimeStart: "2010-06-09T15:20:00+07:00",
+          dateTimeEnd: "2010-06-09T16:20:00+07:00",
+          ruang: "7602"
+        }
+      },
+      myDataSidangTA: {
+        rincian: {
+          topik: "Implementasi X untuk Y",
+          nama: "Ikhwanul Muslimin",
+          nim: 13514020,
+          dosenPembimbing: [
+            'Rinaldi Munir',
+            'Mesayu'
+          ],
+          dosenPengujiAkhir: [
+            'Inggriani Liem',
+            'Bayu Hendrajaya'
+          ]
+        },
+        jadwal: {
+          dateTimeStart: "2010-06-09T15:20:00+07:00",
+          dateTimeEnd: "2010-06-09T16:20:00+07:00",
+          ruang: "7602"
+        }
+      }
+    };
+  }
+
+  handleToggle() {this.setState({open: !this.state.open})};
+
+  handleClose() {this.setState({open: false})};
+
+  render() {
+    return (
+      <MuiThemeProvider>
+      <div>
+        <AppBar
+          title="Halaman Mahasiswa - Jadwal"
+          iconElementLeft={
+            <IconButton tooltip="Menu" onClick = {()=>this.handleToggle()}>
+              <i className="material-icons" style={{color: 'white'}}>menu</i>
+            </IconButton>
+          }
+          iconElementRight={
+            <RaisedButton
+              label="Logout"
+              backgroundColor="#F44336"
+              labelColor= "#fff"
+            />
+          }
+        />
+
+        <p className="tableTitle">Jadwal Seminar Kerja Praktik Anda</p>
+        <br/>
+        <Row className="infoSidang">
+          <Col md="6" xs="12">
+            <Card>
+              <CardTitle title="Rincian Anda"/>
+              <CardText>
+                <Table selectable={false}>
+                  <TableBody displayRowCheckbox={false}>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Kelompok</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSeminarKP.rincian.idKelompok}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Topik</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSeminarKP.rincian.topik}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Nama</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSeminarKP.rincian.nama}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">NIM</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSeminarKP.rincian.nim}</TableRowColumn>
+                    </TableRow>
+                    {this.state.myDataSeminarKP.rincian.dosenPembimbing.map((item, i)=>(
+                    <TableRow key={i}>
+                      <TableRowColumn className="attributeTable">{"Pembimbing "+(i+1)}</TableRowColumn>
+                      <TableRowColumn>{item}</TableRowColumn>
+                    </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </CardText>
+            </Card>
+            <br/>
+          </Col>
+          <Col md="6" xs="12">
+            <Card>
+              <CardTitle title="Jadwal Anda"/>
+              <CardText>
+                <Table selectable={false}>
+                  <TableBody displayRowCheckbox={false}>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Tanggal Sidang</TableRowColumn>
+                      <TableRowColumn>{dateFormat(this.state.myDataSeminarKP.jadwal.dateTimeStart, "dddd, dd mmmm yyyy")}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Waktu</TableRowColumn>
+                      <TableRowColumn>{dateFormat(this.state.myDataSeminarKP.jadwal.dateTimeStart, "HH.MM")+"-"+dateFormat(this.state.myDataSeminarKP.jadwal.dateTimeEnd, "HH.MM")}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Ruang</TableRowColumn>
+                      <TableRowColumn>7601</TableRowColumn>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </CardText>
+            </Card>
+          </Col>
+        </Row>
+        <br/>
+        <p className="tableTitle">Jadwal Seminar TA1</p>
+        <br/>
+        <Row className="infoSidang">
+          <Col md="6" xs="12">
+            <Card>
+              <CardTitle title="Rincian Anda"/>
+              <CardText>
+                <Table selectable={false}>
+                  <TableBody displayRowCheckbox={false}>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Topik</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSeminarTA1.rincian.topik}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Nama</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSeminarTA1.rincian.nama}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">NIM</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSeminarTA1.rincian.nim}</TableRowColumn>
+                    </TableRow>
+                    {this.state.myDataSeminarTA1.rincian.dosenPembimbing.map((item, i)=>(
+                    <TableRow key={i}>
+                      <TableRowColumn className="attributeTable">{"Pembimbing "+(i+1)}</TableRowColumn>
+                      <TableRowColumn>{item}</TableRowColumn>
+                    </TableRow>
+                    ))}
+                    {this.state.myDataSeminarTA1.rincian.dosenPengujiTA1.map((item, i)=>(
+                    <TableRow key={i}>
+                      <TableRowColumn className="attributeTable">{"Penguji "+(i+1)}</TableRowColumn>
+                      <TableRowColumn>{item}</TableRowColumn>
+                    </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </CardText>
+            </Card>
+            <br/>
+          </Col>
+          <Col md="6" xs="12">
+            <Card>
+              <CardTitle title="Jadwal Anda"/>
+              <CardText>
+                <Table selectable={false}>
+                  <TableBody displayRowCheckbox={false}>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Tanggal Sidang</TableRowColumn>
+                      <TableRowColumn>{dateFormat(this.state.myDataSeminarTA1.jadwal.dateTimeStart, "dddd, dd mmmm yyyy")}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Waktu</TableRowColumn>
+                      <TableRowColumn>{dateFormat(this.state.myDataSeminarTA1.jadwal.dateTimeStart, "HH.MM")+"-"+dateFormat(this.state.myDataSeminarTA1.jadwal.dateTimeEnd, "HH.MM")}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Ruang</TableRowColumn>
+                      <TableRowColumn>7601</TableRowColumn>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </CardText>
+            </Card>
+          </Col>
+        </Row>
+        <br/>
+        <p className="tableTitle">Jadwal Seminar TA 2 Anda</p>
+        <br/>
+        <Row className="infoSidang">
+          <Col md="6" xs="12">
+            <Card>
+              <CardTitle title="Rincian Anda"/>
+              <CardText>
+                <Table selectable={false}>
+                  <TableBody displayRowCheckbox={false}>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Topik</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSeminarTA2.rincian.topik}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Nama</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSeminarTA2.rincian.nama}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">NIM</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSeminarTA2.rincian.nim}</TableRowColumn>
+                    </TableRow>
+                    {this.state.myDataSeminarTA2.rincian.dosenPembimbing.map((item, i)=>(
+                    <TableRow key={i}>
+                      <TableRowColumn className="attributeTable">{"Pembimbing "+(i+1)}</TableRowColumn>
+                      <TableRowColumn>{item}</TableRowColumn>
+                    </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </CardText>
+            </Card>
+            <br/>
+          </Col>
+          <Col md="6" xs="12">
+            <Card>
+              <CardTitle title="Jadwal Anda"/>
+              <CardText>
+                <Table selectable={false}>
+                  <TableBody displayRowCheckbox={false}>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Tanggal Sidang</TableRowColumn>
+                      <TableRowColumn>{dateFormat(this.state.myDataSeminarTA2.jadwal.dateTimeStart, "dddd, dd mmmm yyyy")}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Waktu</TableRowColumn>
+                      <TableRowColumn>{dateFormat(this.state.myDataSeminarTA2.jadwal.dateTimeStart, "HH.MM")+"-"+dateFormat(this.state.myDataSeminarTA2.jadwal.dateTimeEnd, "HH.MM")}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Ruang</TableRowColumn>
+                      <TableRowColumn>7601</TableRowColumn>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </CardText>
+            </Card>
+          </Col>
+        </Row>
+        <br/>
+        <p className="tableTitle">Jadwal Sidang Akhir Anda</p>
+        <br/>
+        <Row className="infoSidang">
+          <Col md="6" xs="12">
+            <Card>
+              <CardTitle title="Rincian Anda"/>
+              <CardText>
+                <Table selectable={false}>
+                  <TableBody displayRowCheckbox={false}>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Topik</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSidangTA.rincian.topik}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Nama</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSidangTA.rincian.nama}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">NIM</TableRowColumn>
+                      <TableRowColumn>{this.state.myDataSidangTA.rincian.nim}</TableRowColumn>
+                    </TableRow>
+                    {this.state.myDataSidangTA.rincian.dosenPembimbing.map((item, i)=>(
+                    <TableRow key={i}>
+                      <TableRowColumn className="attributeTable">{"Pembimbing "+(i+1)}</TableRowColumn>
+                      <TableRowColumn>{item}</TableRowColumn>
+                    </TableRow>
+                    ))}
+                    {this.state.myDataSidangTA.rincian.dosenPengujiAkhir.map((item, i)=>(
+                    <TableRow key={i}>
+                      <TableRowColumn className="attributeTable">{"Penguji "+(i+1)}</TableRowColumn>
+                      <TableRowColumn>{item}</TableRowColumn>
+                    </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </CardText>
+            </Card>
+            <br/>
+          </Col>
+          <Col md="6" xs="12">
+            <Card>
+              <CardTitle title="Jadwal Anda"/>
+              <CardText>
+                <Table selectable={false}>
+                  <TableBody displayRowCheckbox={false}>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Tanggal Sidang</TableRowColumn>
+                      <TableRowColumn>{dateFormat(this.state.myDataSidangTA.jadwal.dateTimeStart, "dddd, dd mmmm yyyy")}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Waktu</TableRowColumn>
+                      <TableRowColumn>{dateFormat(this.state.myDataSidangTA.jadwal.dateTimeStart, "HH.MM")+"-"+dateFormat(this.state.myDataSidangTA.jadwal.dateTimeEnd, "HH.MM")}</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                      <TableRowColumn className="attributeTable">Ruang</TableRowColumn>
+                      <TableRowColumn>7601</TableRowColumn>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </CardText>
+            </Card>
+          </Col>
+        </Row>
+        <Drawer
+          docked={false}
+          width={400}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+        <IconButton tooltip="Tutup" onClick = {()=>this.handleClose()}>
+          <i className="material-icons" style={{color: 'white'}}>close</i>
+        </IconButton>
+          <div className="userProfile">
+            <Row>
+              <Col md="3" xs="2">
+                <img src={imgProfile} className="imgProfile"/>
+              </Col>
+              <Col md="9" xs="10" className="textProfile">
+                <Row>
+                  <Col className="nameProfile">{this.state.userInfo.nama}</Col>
+                  <Col className="emailProfile">{this.state.userInfo.email}</Col>
+                  <Col className="emailProfile">{this.state.userInfo.peran}</Col>
+                </Row>
+              </Col>
+            </Row>
+          </div>
+          <hr/>
+          <MenuItem insetChildren={true} href="/mhs_jadwal" style={{backgroundColor:'#b0bec5'}}>Jadwal</MenuItem>
+          <hr/>
+          <MenuItem insetChildren={true} href="/mhs_profile">Profil</MenuItem>
+
+          <br/>
+        </Drawer>
+      </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default mhs_jadwal;
