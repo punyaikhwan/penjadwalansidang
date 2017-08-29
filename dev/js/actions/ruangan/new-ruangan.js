e9a297f1970f962bab3ab5afa659b41f958ddbe5
Hasna <hasnurk@gmail.com>
2017-08-29 07:55:31 +0700

    ruangan functions

--- dev/js/actions/ruangan/new-ruangan.js
+++ dev/js/actions/ruangan/new-ruangan.js
@@ -1,3 +1,29 @@
-/**
- * Created by Lenovo PC on 08/29/2017.
- */
+import axios from 'axios'
+
+export const newRuangan= (nama) => {
+    return function(dispatch) {
+
+        dispatch({
+            type: "NEW RUANGAN"
+        })
+        axios.post('http://localhost:3001/ruangan/new', {
+            nama: nama
+        }).then(function (data) {
+
+            dispatch({
+                type: "DONE NEW RUANGAN"
+
+            })
+            axios.get('http://localhost:3001/ruangan').then(function (data) {
+                console.log(data.data)
+
+                dispatch({
+                    type: "DONE FETCH RUANGAN",
+                    payload: data.data
+
+                })
+            })
+        })
+
+    }
+}
--- dev/js/reducers/ruangan.js
+++ dev/js/reducers/ruangan.js
@@ -1,6 +1,10 @@
 export default function (state = [], action) {
     switch (action.type) {
         case 'DONE FETCH RUANGAN':
+            action.payload.forEach(function(item) {
+                console.log(item);
+                item.isEdit = 0;
+            })
             return action.payload;
             break;
     }
